d8a7b4b08f686a22853efb49628c348d8aeb01d3
Bari Cok <neon03-uu@yandex.ru>
2018-07-16 22:21:45 +0800

    setting new bd

--- nivaMain/settings_postgres.py
+++ nivaMain/settings_postgres.py
@@ -1,12 +1,12 @@
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'auto_bd',
-        'USER': 'auto_blog',
-        'PASSWORD': '1awdw00DDru84#2',
+        'NAME': 'auto_bdsm',
+        'USER': 'auto_blogs',
+        'PASSWORD': '1awdw00DDru',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
